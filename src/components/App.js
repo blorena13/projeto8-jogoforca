3e78e9081a8f20e414b4ca24cd006f38c85889d1
Lorena <blorena1321@gmail.com>
2023-02-14 15:52:58 -0300

    componentes

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import Jogo from "./Jogo";
+import Letras from "./Letras";
+import palavras from "../palavras";
+
+function App() {
+  return (
+    <div>
+      <Jogo />
+      <Letras />
+    </div>
+  );
+}
+
+export default App;
